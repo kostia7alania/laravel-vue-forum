b33eb18c60609d9f640500df25774da4aafaf812
kostia7alania <osetia-alania@mail.ru>
2019-03-29 14:28:29 +0300

    fix column type

--- database/migrations/2019_03_29_090005_create_likes_table.php
+++ database/migrations/2019_03_29_090005_create_likes_table.php
@@ -14,7 +14,7 @@ class CreateLikesTable extends Migration
     public function up()
     {
         Schema::create('likes', function (Blueprint $table) {
-            $table->integer('id');
+            $table->increments('id');
             $table->integer('reply_id');
             $table->integer('user_id');
             $table->timestamps();
