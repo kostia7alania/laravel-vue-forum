4894406cca5aecb8a4041b379f367bba60a7ce40
kostia7alania <osetia-alania@mail.ru>
2019-03-31 22:59:31 +0300

    CRUD 4 Question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Model\Question;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
@@ -14,18 +16,11 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+             //echo phpinfo();die;
+        return QuestionResource::collection(Question::latest()->get());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -35,7 +30,10 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      //auth()->user()->question()->create($request->all());
+      Question::create($request->all());
+      return response('Created', Response::HTTP_CREATED);
+
     }
 
     /**
@@ -46,19 +44,9 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return new QuestionResource($question);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -80,6 +68,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,13 @@ use App\User;
 
 class Question extends Model
 {
+    public function getRouteKeyName() {
+        return 'slug';//берем из столбца слог
+    }
+
+    //protected $fillable = ['title','slug','body','category_id','user_id'];
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -21,5 +28,8 @@ class Question extends Model
         return $this->belongsTo('Category::class');
     }
 
+    public function getPathAttribute() {
+        return asset("api/question/$this->slug");
+    }
 
 }
