8536a4fc538fab5b92652f44a3420c3c76a22d07
kostia7alania <osetia-alania@mail.ru>
2019-04-17 19:52:06 +0300

    => date_time -> to front "side rendering" ;

--- app/Http/Resources/QuestionTopicsResource.php
+++ app/Http/Resources/QuestionTopicsResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class QuestionTopicsResource extends JsonResource
 {
@@ -21,7 +22,8 @@ class QuestionTopicsResource extends JsonResource
             'reply_count' => $this->replies->count(),
             'path' => $this->path,
             'body' => $this->body,
-            'created_at' => $this->created_at->diffForHumans(),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
             'user' => $this->user->name,
             'id' => $this->user_id,
         ];
