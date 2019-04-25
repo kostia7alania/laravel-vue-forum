9bf08d2648a60e1b263053852b7c54ccd3178006
kostia7alania <osetia-alania@mail.ru>
2019-04-25 16:22:33 +0300

    err handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -12,39 +12,15 @@ use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+    /** * A list of the exception types that are not reported.  * @var array */
+    protected $dontReport = [  ];
+    /** A list of the inputs that are never flashed for validation exceptions. @var array */
+    protected $dontFlash = [ 'password', 'password_confirmation', ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+    /**  Report or log an exception.  * @param  \Exception  $exception * @return void  */
+    public function report(Exception $exception)  {  parent::report($exception);  }
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
+    /** * Render an exception into an HTTP response.
      * @param  \Illuminate\Http\Request  $request
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
