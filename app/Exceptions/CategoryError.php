613649e86e8c5ab5c55a61983ed06b800112587e
kostia7alania <osetia-alania@mail.ru>
2019-05-29 10:29:53 +0300

    category valid

---
+++ app/Exceptions/CategoryError.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Exceptions\CategoryError;
+
+class Handler extends ExceptionHandler
+{   /** * A list of the exception types that are not reported. ** @var array */
+    protected $dontReport = [     ];
+    /*** A list of the inputs that are never flashed for validation exceptions.** @var array*/
+    protected $dontFlash = [ 'password', 'password_confirmation', ];
+
+    /** * Report or log an exception. * * @param  \Exception  $exception * @return void */
+    public function report(Exception $exception) { parent::report($exception); }
+    /** * Render an exception into an HTTP response. * * @param  \Illuminate\Http\Request  $request * @param  \Exception  $exception * @return \Illuminate\Http\Response */
+    public function render($request, Exception $exception)
+    {
+        dd($exception); //return redirect('/');
+        if($exception instanceof CategoryError) {
+            return response(['error' => 'Token can not be used, get new one'], Response::HTTP_BAD_REQUEST);
+        }
+        return parent::render($request, $exception);
+    }
+}
