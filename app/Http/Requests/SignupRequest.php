0ece32cfd673747fa53e69571a8578c4b3a21f4e
kostia7alania <osetia-alania@mail.ru>
2019-06-01 12:24:06 +0300

    signup validation

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -15,7 +15,7 @@ class SignupRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
+            'name' => 'required|max:255',
             'email' => 'required|unique:users|email',
             'password'=>'required|confirmed'
         ];
