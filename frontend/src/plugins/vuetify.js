322863a491a3f17c9f5e4fe4fbcda6f7b3de6373
kostia7alania <osetia-alania@mail.ru>
2019-06-03 18:42:49 +0300

    add vuetify themes

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,7 +4,15 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'mdi',
-  },
+    icons: {
+        theme: {
+            dark: true,
+            themes: {
+                light:{ primary: '#42a5f5' },
+                dark: { primary: '#2196F3' },
+            },
+        },
+        options: { customProperties: true },
+        iconfont: 'mdi',
+    },
 });
