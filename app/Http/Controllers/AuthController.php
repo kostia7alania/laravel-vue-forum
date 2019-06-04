47d57d8bff4d94c8e63e0a758afe6c2bd65d1ced
kostia7alania <osetia-alania@mail.ru>
2019-06-04 17:54:41 +0300

    login with user role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -52,14 +52,13 @@ class AuthController extends Controller
     }
 
     /** Get the token array structure.  @param  string $token  * @return \Illuminate\Http\JsonResponse  */
-    protected function respondWithToken($token)
-    {
+    protected function respondWithToken($token) {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
             'user' => auth()->user()->name,
-            'role' => auth()->user()->role
+            'roles' => auth()->user()->roles()->select('name')->get()
         ]);
     }
 }
--- app/Model/Role.php
+++ app/Model/Role.php
@@ -1,15 +1,14 @@
 <?php
-
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
-{
+class Role extends Model {
 
     protected $fillable = ['name'];
+
     public function users() {
-      return $this->belongsToMany('App\User', 'user_roles');
+        return $this->belongsToMany('App\User', 'user_roles');
     }
 
 }
