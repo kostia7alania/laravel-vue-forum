4177b6ef4f7cee1d08b2f830b2d9e7bcc13f2a58
kostia7alania <osetia-alania@mail.ru>
2019-06-05 12:38:32 +0300

    reply filter

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -12,6 +12,7 @@ use App\Notifications\NewReplyNotification;
 use App\Events\DeleteReplyEvent;
 use App\Http\Requests\ReplyRequest;
 use PHPUnit\Framework\Constraint\Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ReplyController extends Controller
 {
@@ -37,18 +38,29 @@ class ReplyController extends Controller
     public function store(Question $question, ReplyRequest $request)
     {
 
+    try {
+        /* ==> EXAMPLE::
+          $admin = User::find($request->email);
+          $office = Office::create($request->all() + ['admin_id' => $admin->id]);
+          (new UserService())->assignAdminToOffice($office);
+        */
 
-       // $question = auth()->user()->question()->replies()->create($request->all());
+      //  $question = auth()->user()->question()->replies()->create($request->all());
+            $reply = $question->replies()->create([//create($request->all());
+                'user_id' => auth()->id(),
+                'body' => $request->body,//test
+            ]);
+            $user = $question->user;
+            if($reply->user_id!==$question->user_id) {
+            //  $user->notify(new NewReplyNotification($reply));
+            }
+            return response(['Reply' => new ReplyResource($reply)], Response::HTTP_CREATED);
 
-        $reply = $question->replies()->create([//create($request->all());
-            'user_id' => auth()->id(),
-            'body' => $request->body,//test
-        ]);
-        $user = $question->user;
-        if($reply->user_id!==$question->user_id) {
-            $user->notify(new NewReplyNotification($reply));
+        } catch (ModelNotFoundException $ex) { // User not found
+            abort(422, 'Invalid model - not found');
+        } catch (Exception $ex) { // Anything that went wrong
+            abort(500, 'Could not assign');
         }
-        return response(['Reply' => new ReplyResource($reply)], Response::HTTP_CREATED);
     }
 
     /*  Display the specified resource.
