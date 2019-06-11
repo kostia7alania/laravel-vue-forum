fecdd00126fc4b71693426cb8f24d17116a79d2c
kostia7alania <osetia-alania@mail.ru>
2019-06-11 17:12:54 +0300

    add jest

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -1,11 +1,11 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from '@/components/App.vue'
 
-describe('HelloWorld.vue', () => {
+describe('App.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(App, {
       propsData: { msg }
     })
     expect(wrapper.text()).to.include(msg)
