f28c60dce24305df50152991493e76437aa5690d
kostia7alania <osetia-alania@mail.ru>
2019-06-20 18:26:20 +0300

    works with login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,12 @@ use App\Http\Requests\SignupRequest;
 
 use Illuminate\Support\Facades\DB;
 
+// use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
+use Tymon\JWTAuth\PayloadFactory;
+
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
@@ -22,13 +28,41 @@ class AuthController extends Controller
     }
 
     /**  Get a JWT via given credentials. @return \Illuminate\Http\JsonResponse  */
-    public function login() {
+    public function login(Request $request) {
+    /*
         $credentials = request(['email', 'password']);
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
         return $this->respondWithToken($token);
+    */
+
+        $credentials = $request->only('email', 'password');
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if ( !$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) { // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+
+        // grab some user
+ //   $user = User::first();
+//    $token = JWTAuth::fromUser($user);
+
+    /*
+    $customClaims = ['sex' => 'bar', 'fuuckk' => 'bob'];
+    $payload = PayloadFactory::make($customClaims);
+    $token = JWTAuth::encode($payload);
+    print_r($token);
+    die;
+    */
+    return response()->json($token); // all good so return the token
+        //return $this->respondWithToken($token);
     }
+
 
     public function signup(SignupRequest $request) {
         User::create($request->all());
@@ -37,7 +71,13 @@ class AuthController extends Controller
 
     /**  Get the authenticated User.  * @return \Illuminate\Http\JsonResponse  */
     public function me() {
-        return response()->json(auth()->user());
+        //return response()->json(auth()->user());
+        $user = JWTAuth::user();
+        if (count((array)$user) > 0) {
+            return response()->json(['status' => 'success', 'user' => $user]);
+        } else {
+            return response()->json(['status' => 'fail'], 401);
+        }
     }
 
     /**  Log the user out (Invalidate the token).  @return \Illuminate\Http\JsonResponse */
@@ -53,7 +93,16 @@ class AuthController extends Controller
 
     /** Get the token array structure.  @param  string $token  * @return \Illuminate\Http\JsonResponse  */
     protected function respondWithToken($token) {
+        //$payload = JWTFactory::sub(123)->aud('foo')->foo(['bar' => 'baz'])->make();
+        /*
+
+        $customClaims = ['foo' => 'bar', 'baz' => 'bob'];
+        $payload = PayloadFactory::make($customClaims);
+        print_r($payload);die;
+        */
+
         return response()->json([
+            //'token2'=>$token2,
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
