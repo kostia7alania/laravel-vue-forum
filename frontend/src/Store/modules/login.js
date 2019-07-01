d9e1a7d21e49cba9c4512526196a1aa1d2c41177
kostia7alania <osetia-alania@mail.ru>
2019-07-01 15:49:44 +0300

    FIX token errors

--- frontend/src/Store/modules/login.js
+++ frontend/src/Store/modules/login.js
@@ -1,22 +1,25 @@
 import Vue from 'vue'
 // import AppStorage from '../Helpers/AppStorage';
-export default  {
+export default {
 
-    state:  {
+    state: {
         token: null,//AppStorage.getToken(),
         tokenValidation: {
             iss: "http://localhost/api/auth/login"
         },
         loading: false,
-        now: new Date
+        now: new Date,
+        user: {
+            name: null,
+            role: null,
+        },
     },
 
     getters: {
 
-        TOKEN__isValid (state, getters) {
+        TOKEN__isValid(state, getters) {
             const isBase64 = str => {
-                try {  return btoa(atob(str)).replace(/=/g, '') == str } catch (err)
-                { return false }
+                try { return btoa(atob(str)).replace(/=/g, '') == str } catch (err) { return false }
             }
             const decode = payload => isBase64(payload) ? JSON.parse(atob(payload)) : false
             const token = state.token;
@@ -26,13 +29,13 @@ export default  {
             }
             return false;
         },
-        TOKEN__isExpired(state,getters) {
-            if(getters.TOKEN__parse)
-                return state.now > getters.TOKEN__parse.exp*1000    //да-истек, нет-не истек
+        TOKEN__isExpired(state, getters) {
+            if (getters.TOKEN__parse)
+                return state.now > getters.TOKEN__parse.exp * 1000    //да-истек, нет-не истек
             else return null
         },
-        TOKEN__parse(state,getters) {
-            if(getters.TOKEN__isValid) {
+        TOKEN__parse(state, getters) {
+            if (getters.TOKEN__isValid) {
                 const parts = state.token.split('.');
                 return {
                     ...JSON.parse(atob(parts[0])),
@@ -41,38 +44,32 @@ export default  {
             }
         },
 
-        TOKEN__isOK(state,getters) {
+        TOKEN__isOK(state, getters) { // токен валиден + действителен!!!
             return !getters.TOKEN__isExpired && getters.TOKEN__isValid
         },
 
-        id(state,getters) {
-            if(getters.TOKEN__parse)
-            return getters.TOKEN__parse.sub;// токен содержит user_id || id
+        id(state, getters) {
+            if (getters.TOKEN__parse)
+                return getters.TOKEN__parse.sub;// токен содержит user_id || id
         },
 
-        isAdmin(state,getters) {
+        isAdmin(state, getters) {
             return getters.TOKEN__isOK //&& state.user.role == 1 //0-user, 1-admin, null - first reg;
         },
 
-        isUser(state,getters) {
+        isUser(state, getters) {
             return getters.TOKEN__isOK //&&  (state.user.role === null || state.user.role === 0)  //0-user, 1-admin, null - first reg;
         },
-
-        username(state,getters) {
-            return state.user.name
+        username(state, getters) {
+            return state.user.name || 'не авторизован'
         },
-
-        loggedIn(state,getters) {
-            return getters.isUser || getters.isAdmin
-        },
-
-
+        loggedIn(state, getters) { return getters.TOKEN__isOK },
     },
 
     mutations: { /***** USING GLOBAL ****/
         toggle(state, { prop }) { state[prop] = !state[prop]; },
-        changeProp(state, { prop, val } ) { state[prop] = val },
-        changeObj(state, { obj, prop, val } ) { state[obj][prop] = val },
+        changeProp(state, { prop, val }) { state[prop] = val },
+        changeObj(state, { obj, prop, val }) { state[obj][prop] = val },
 
 
         SET_LOADING_ON(state) { state.loading = true },
@@ -84,60 +81,59 @@ export default  {
         },
         DROP_TOKEN(state) {
             state.token = null
-            state.user.role = null
         },
 
-        UPDATE_DATE (state) { state.now = +new Date }
+        UPDATE_DATE(state) { state.now = +new Date }
     },
 
     actions: {
-        startUpdatingTime ({ commit, dispatch }) {
+        startUpdatingTime({ commit, dispatch }) {
             setInterval(() => {
                 commit('UPDATE_DATE')
                 console.log('Date updated ')
                 dispatch('checkPermitionsOnCurrentPath')
-            }, 1000 * 3 )
+            }, 1000 * 3)
         },
-        async login( {state, commit, dispatch}, logPass) {
+        async login({ state, commit, dispatch }, logPass) {
             commit('SET_LOADING_ON')
             return axios
-                        .post(`auth/login`, logPass)
-                        .then( res => { commit('SET_TOKEN', res.data.access_token); return res.data; } )
-                        .catch( err => err )
-                        .finally(() => {
-                            commit('SET_LOADING_OFF')
-                            dispatch('checkPermitionsOnCurrentPath')
-                        })
+                .post(`auth/login`, logPass)
+                .then(res => { commit('SET_TOKEN', res.data.access_token); return res.data; })
+                .catch(err => err)
+                .finally(() => {
+                    commit('SET_LOADING_OFF')
+                    dispatch('checkPermitionsOnCurrentPath')
+                })
         },
 
-        async register( { commit, dispatch }, logPass) {
+        async register({ commit, dispatch }, logPass) {
             commit('SET_LOADING_ON')
             return axios
-                        .post(`auth/signup`, logPass)
-                        .then( res => { commit('SET_TOKEN', res.data.access_token); return true;} )
-                        .catch( err => err.response && err.response.data && err.response.data.errors )
-                        .finally(() => {
-                            commit('SET_LOADING_OFF')
-                            dispatch('checkPermitionsOnCurrentPath')
-                        })
+                .post(`auth/signup`, logPass)
+                .then(res => { commit('SET_TOKEN', res.data.access_token); return true; })
+                .catch(err => err.response && err.response.data && err.response.data.errors)
+                .finally(() => {
+                    commit('SET_LOADING_OFF')
+                    dispatch('checkPermitionsOnCurrentPath')
+                })
         },
 
-        logout({commit,dispatch}) {
+        logout({ commit, dispatch }) {
             commit('DROP_TOKEN')
             dispatch('checkPermitionsOnCurrentPath')
-         },
+        },
 
-        checkPermitionsOnCurrentPath({state}, to=router.history.current.path) {
+        checkPermitionsOnCurrentPath({ state }, to = router.history.current.path) {
             //debugger
             const role = store.state.login.user.role // state.user.role;
-            const path = store.getters['toolbar/items'].filter(e=>e['to']==to)
-            if(path.length) {
-                if(path[0].roles.includes(role) || path[0].roles.includes('*')){
+            const path = store.getters['toolbar/items'].filter(e => e['to'] == to)
+            if (path.length) {
+                if (path[0].roles.includes(role) || path[0].roles.includes('*')) {
                     console.warn('есть права')
                     return true
                 } else {
                     console.warn('нет прав!!')
-                    router.push({name:'forum'})
+                    router.push({ name: 'forum' })
                     return false
                 }
             }
--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -2,11 +2,11 @@
     <v-app class="app">
         <toolbar></toolbar>
 
- <keep-alive max="1">
-     <transition name="fade">
-        <router-view :key="$route.fullPath"/>
-    </transition>
- </keep-alive>
+        <keep-alive max="1">
+            <transition name="fade">
+                <router-view :key="$route.fullPath"/>
+            </transition>
+        </keep-alive>
 
         <snack/>
         <v-spacer></v-spacer>
@@ -15,35 +15,27 @@
 </template>
 
 <script>
-import toolbar from './Toolbar'
-import snack from '@/components/global/snack'
-import AppFooter from './AppFooter'
-
-import { mapGetters, mapActions } from 'vuex'
+import toolbar from "./Toolbar";
+import snack from "@/components/global/snack";
+import AppFooter from "./AppFooter";
 
 export default {
-    name: "APP__HOME",
-    components: { toolbar,'foot-app': AppFooter, snack },
-    computed: {
-        ...mapGetters([ 'login/TOKEN__isOK' ])
-    },
-    mounted() {
-        window.app = this;
-        if( !this['login/TOKEN__isOK']) this['login/logout']();
-    },
-    methods: {
-        ...mapActions([ 'login/logout' ])
-    }
-}
+  name: "APP__HOME",
+  components: {
+    toolbar,
+    "foot-app": AppFooter,
+    snack
+  }
+};
 </script>
 
 <style>
 .app {
-    height: 100%;;
+  height: 100%;
 }
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
   opacity: 0;
